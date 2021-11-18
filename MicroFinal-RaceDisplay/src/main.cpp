d6a3a330d993546a759cafa9e87eb434e7666e0b
thesupermonkey42 <thesupermonkey42@users.noreply.github.com>
2021-11-18 09:47:47 -0600

    Display functions implemented

--- MicroFinal-RaceDisplay/src/main.cpp
+++ MicroFinal-RaceDisplay/src/main.cpp
@@ -1,44 +1,5 @@
 #include "pixel_display.hpp"
 
-
-/* Usable Functions:
-
- draw a pixel in solid white
-matrix.drawPixel(0, 0, matrix.Color333(7, 7, 7));
-
- fix the screen with green
-matrix.fillRect(0, 0, 32, 16, matrix.Color333(0, 7, 0));
-
- draw a box in yellow
-matrix.drawRect(0, 0, 32, 16, matrix.Color333(7, 7, 0));
-
- draw an 'X' in red
-matrix.drawLine(0, 0, 31, 15, matrix.Color333(7, 0, 0));
-matrix.drawLine(31, 0, 0, 15, matrix.Color333(7, 0, 0));
-
- draw a blue circle
-matrix.drawCircle(7, 7, 7, matrix.Color333(0, 0, 7));
- 
- fill a violet circle
-matrix.fillCircle(23, 7, 7, matrix.Color333(7, 0, 7));
-
- fill the screen with 'black'
-matrix.fillScreen(matrix.Color333(0, 0, 0));
-
-// draw some text!
-matrix.setTextSize(1);     // size 1 == 8 pixels high
-matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
-
-matrix.setCursor(8, 0);    // start at top left, with 8 pixel of spacing
-uint8_t w = 0;
-char *str = "AdafruitIndustries";
-for (w=0; w<8; w++) {
-  matrix.setTextColor(Wheel(w));
-  matrix.print(str[w]);
-}
-
-*/
-
 /* DON'T DELETE YET. Method 1 for active timer.
 unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= 250) {
@@ -47,16 +8,15 @@ unsigned long currentMillis = millis();
 
     displayTimers(raceTimer);
   }
+
+
+unsigned long currentMillis = millis();
+unsigned long previousMillis = 0;
+unsigned long raceTimer = 0; // Must reset when race is done. When displaying results?
 */
 
-char inByte = 0;
-//char zeroTimersMsg[] = {'0','.','0', '0', '0'};
-//unsigned long currentMillis = millis();
-//unsigned long previousMillis = 0;
-//unsigned long raceTimer = 0; // Must reset when race is done. When displaying results?
-
 void startupMessage() { // Display once in void setup()
-  matrix.setTextColor(matrix.Color888(255,255,255));
+  matrix.setTextColor(matrix.Color888(252,255,255));
   matrix.fillScreen(matrix.Color333(0, 0, 0)); // Blank screen
   matrix.setTextSize(1);
   matrix.setCursor(2,4);
@@ -151,15 +111,7 @@ void finalTimes(unsigned long lane1Millis, unsigned long lane2Millis) {
   matrix.print(lane2Millis-((lane2Millis/1000)*1000));
 }
 
-// Reads data from serial port and prints it on the display. Add Serial.begin(9600) to void setup() to use.
-void serialEcho() {
-  if(Serial.available() > 0) {
-    inByte = Serial.read();
-   matrix.print(inByte);
-  }
-}
-
-void pixel_setup()
+void setup()
 {
   
   matrix.begin();
